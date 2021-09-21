1658bd7e6ecec70570906a4d3271a14843ff4cae
Elliott7 <elliottbreadmore@hotmail.com>
2021-09-21 14:00:17 +1000

    Added call to action component

---
+++ src/components/callToAction/CallToAction.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from './CallToAction.module.css'
+import Button from "../UI/Button"
+
+function CallToAction(props){
+    return(
+        <section className={`${style.mainContainer}`}>
+            <div className={`${style.container} ${props.theme.secondaryBg}`}>
+                <div>
+                    <p className='headings-text text-size-2'>Want more inspirational quotes?</p>
+                    <p className='secondary-text text-size-1'>Of course you do!</p>
+                </div>
+
+                <Button className={`${props.theme.secondaryBg} ${style.button}`} >Click For More</Button>
+            </div>
+
+        </section>
+    )
+}
+
+export default CallToAction
