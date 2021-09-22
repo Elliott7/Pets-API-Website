6d6d00588bda8829b4a769aae61f7c48c710bec2
Elliott7 <elliottbreadmore@hotmail.com>
2021-09-22 12:59:55 +1000

    Split data into its own file and added new data options

--- src/App.js
+++ src/App.js
@@ -6,69 +6,13 @@ import Carousel from "./components/carousel/Carousel";
 import Quote from "./components/quote/Quote";
 import CallToAction from "./components/callToAction/CallToAction";
 import Footer from "./components/footer/Footer";
-
-const dogData = {
-    "themeBackground": "dark-background",
-    "secondaryBg":"dark-background-secondary",
-    "border": "dark-secondary-border",
-    "logoLocation": ["/images/logo4.png", "A happy Shibu Inu",
-        "/images/logo3.png", "Cartoon Shibu Inu in a suit"],
-    "heroPicture": ["/images/happyshib.png", "Two smiling Shibu Inu dogs"],
-    "carouselPictures":[
-        {'src':"/images/nicepupper.jpeg",
-            'alt': "Ecstatic Shiba Inu puppy going for a walk",
-            'desc': "Happy Boi going for a walk"},
-        {'src':"/images/raspberrypupper.jpeg",
-            'alt': "Beautiful Shiba Inu puppy blowing a raspberry",
-            "desc": "Naughty pupper blowing a raspberry"},
-        {'src':"/images/tiredpupper.jpeg",
-            'alt': "Yawning Shibu Inu tired after a long walk",
-            "desc": "Sleepy pupper after a long walk"},
-        {'src':"/images/contentpupper.jpeg",
-            'alt': "Smiling Shibu Inu puppy with a pumpkin on his head",
-            "desc" :"Content pupper with pumpkin hat"}
-    ],
-    "footerLogoLocation": "",
-    "quote": " Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof",
-    "animal": "Doge",
-    "textColor": 'dark-text',
-    "copy": "Copyright Shiba Corp Pty Ltd",
-    "slogan": "Dogs are the best"
-}
-
-const catData = {
-    "themeBackground": "light-background",
-    "secondaryBg": "light-background-secondary",
-    "border": "light-secondary-border",
-    "logoLocation": ["/images/catlogo1.jpeg", "A beautiful Ragdoll cat",
-        "/images/overhangingcat.png", "Overhanging Kitten logo"],
-    "heroPicture": ["/images/catlogo2.jpeg", "cute"],
-    "carouselPictures":[
-        {'src':"/images/cutecat5.jpeg",
-            'alt': "Beautiful Ragdoll cat",
-            'desc': "Beautiful Ragdoll Cat"},
-        {'src':"/images/cutecat2.jpeg",
-            'alt': "Beautiful Ragdoll cat",
-            'desc': "Beautiful Ragdoll Cat"},
-        {'src':"/images/cutecat3.jpeg",
-            'alt': "Beautiful Ragdoll cat",
-            'desc': "Beautiful Ragdoll Cat"},
-        {'src':"/images/cutecat4.jpeg",
-            'alt': "Beautiful Ragdoll cat",
-            'desc': "Beautiful Ragdoll Cat"},
-    ],
-    "footerLogoLocation": "",
-    "quote": "If I fits I sits",
-    "animal": "Kitteh",
-    "textColor": 'light-text',
-    "copy": "Copyright Emperor Kitty Pty Ltd",
-    "slogan": "Bow down to your overlords"
-}
+import {catData, dogData, birdData} from "./components/Data";
 
 
 function App() {
     const [theme, setTheme] = useState(dogData)
     const [url, setUrl] = useState('')
+    const randThemes = [birdData]
 
     let dogAPILInks = []
 
@@ -80,6 +24,11 @@ function App() {
         setUrl('')
         setTheme(catData)
     }
+    const randomTheme = () => {
+        setUrl('')
+        let ind = Math.floor(Math.random()*randThemes.length)
+        setTheme(randThemes[ind])
+    }
     const onGetApi = (response) => {
         setUrl(response)
     }
@@ -90,7 +39,7 @@ function App() {
 
     return (
       <div className="App">
-          <Header theme={theme} changeToDog={dogTheme} changeToCat={catTheme} />
+          <Header theme={theme} changeToDog={dogTheme} changeToCat={catTheme} random={randomTheme} />
           <Hero theme={theme} onApiSave={onGetApi} onDogLoadApi={onDogLoadApi} />
           <Carousel theme={theme} url={url}/>
           <Quote theme={theme} />
---
+++ src/components/Data.js
@@ -0,0 +1,85 @@
+export const dogData = {
+    "animal": "Doge",
+    "themeBackground": "dark-background",
+    "secondaryBg":"dark-background-secondary",
+    "border": "dark-secondary-border",
+    "textColor": 'dark-text',
+    "logoLocation": ["/images/logo4.png", "A happy Shibu Inu",
+        "/images/logo3.png", "Cartoon Shibu Inu in a suit"],
+    "heroPicture": ["/images/happyshib.png", "Two smiling Shibu Inu dogs"],
+    "carouselPictures":[
+        {'src':"/images/nicepupper.jpeg",
+            'alt': "Ecstatic Shiba Inu puppy going for a walk",
+            'desc': "Happy Boi going for a walk"},
+        {'src':"/images/raspberrypupper.jpeg",
+            'alt': "Beautiful Shiba Inu puppy blowing a raspberry",
+            "desc": "Naughty pupper blowing a raspberry"},
+        {'src':"/images/tiredpupper.jpeg",
+            'alt': "Yawning Shibu Inu tired after a long walk",
+            "desc": "Sleepy pupper after a long walk"},
+        {'src':"/images/contentpupper.jpeg",
+            'alt': "Smiling Shibu Inu puppy with a pumpkin on his head",
+            "desc" :"Content pupper with pumpkin hat"}
+    ],
+    "quote": " Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof Woof",
+    "copy": "Copyright Shiba Corp Pty Ltd",
+    "slogan": "Dogs are the best"
+}
+
+export const catData = {
+    "animal": "Kitteh",
+    "themeBackground": "light-background",
+    "secondaryBg": "light-background-secondary",
+    "border": "light-secondary-border",
+    "textColor": 'light-text',
+    "logoLocation": ["/images/catlogo1.jpeg", "A beautiful Ragdoll cat",
+        "/images/overhangingcat.png", "Overhanging Kitten logo"],
+    "heroPicture": ["/images/catlogo2.jpeg", "cute"],
+    "carouselPictures":[
+        {'src':"/images/cutecat5.jpeg",
+            'alt': "Beautiful Ragdoll cat",
+            'desc': "Beautiful Ragdoll Cat"},
+        {'src':"/images/cutecat2.jpeg",
+            'alt': "Beautiful Ragdoll cat",
+            'desc': "Beautiful Ragdoll Cat"},
+        {'src':"/images/cutecat3.jpeg",
+            'alt': "Beautiful Ragdoll cat",
+            'desc': "Beautiful Ragdoll Cat"},
+        {'src':"/images/cutecat4.jpeg",
+            'alt': "Beautiful Ragdoll cat",
+            'desc': "Beautiful Ragdoll Cat"},
+    ],
+    "quote": "If I fits I sits",
+    "copy": "Copyright Emperor Kitty Pty Ltd",
+    "slogan": "Bow down to your overlords"
+}
+
+
+export const birdData = {
+    "animal": "Bird",
+    "themeBackground": "green-background",
+    "secondaryBg": "green-background-secondary",
+    "border": "green-secondary-border",
+    "textColor": 'green-text',
+    "logoLocation": ["/images/birds/logo1.jpeg", "A stunning Macaw Bird",
+        "/images/birds/bird1.png", "Overhanging bird logo"],
+    "heroPicture": ["/images/birds/birdC3.jpeg", "cute"],
+    "carouselPictures":[
+        {'src':"/images/birds/birdC2.jpeg",
+            'alt': "",
+            'desc': ""},
+        {'src':"/images/birds/birdC1.jpeg",
+            'alt': "Two colourful Macaws. A yellow blue Macaw grooming a red Macaw",
+            'desc': ""},
+        {'src':"/images/birds/birdC4.jpeg",
+            'alt': "",
+            'desc': ""},
+        {'src':"/images/birds/birdC5.jpeg",
+            'alt': "",
+            'desc': ""},
+    ],
+    "quote": "I want a Cracker?",
+    "copy": "Copyright Squawks Inc",
+    "slogan": "Squawk"
+}
+
